0ee24997bd87871b4dc50031e6ed38e9ce03368b
Iori Yanokura <ab.ioryz@gmail.com>
2024-12-10 21:00:31 +0900

    Add compressed urdf file's md5sum and ues it to check downloading

--- ros/kxr_models/python/kxr_models/download_urdf.py
+++ ros/kxr_models/python/kxr_models/download_urdf.py
@@ -1,10 +1,14 @@
+from distutils.version import StrictVersion
 import os.path as osp
 
 import gdown
+import pkg_resources
 import rospkg
 import rospy
 
 from kxr_models.ros import get_namespace
+
+gdown_version = pkg_resources.get_distribution("gdown").version
 
 
 def download_urdf_mesh_files(namespace=None):
@@ -14,11 +18,15 @@ def download_urdf_mesh_files(namespace=None):
     server_ip = rospy.get_param(namespace + "/model_server_ip", "localhost")
 
     urdf_hash = rospy.get_param(namespace + "/urdf_hash", None)
+    compressed_urdf_hash = rospy.get_param(
+        namespace + "/compressed_urdf_hash", None)
     rate = rospy.Rate(1)
-    while not rospy.is_shutdown() and urdf_hash is None:
+    while not rospy.is_shutdown() and urdf_hash is None and compressed_urdf_hash is None:
         rate.sleep()
-        rospy.loginfo("Waiting rosparam {} set".format(namespace + "/urdf_hash"))
+        rospy.loginfo("Waiting rosparam {} and {} set".format(
+            namespace + "/urdf_hash", namespace + "/compressed_urdf_hash"))
         urdf_hash = rospy.get_param(namespace + "/urdf_hash", None)
+        compressed_urdf_hash = rospy.get_param(namespace + "/compressed_urdf_hash", None)
 
     server_url = f"http://{server_ip}:{port}/urdf/{urdf_hash}.tar.gz"
 
@@ -31,7 +39,12 @@ def download_urdf_mesh_files(namespace=None):
     while not osp.exists(compressed_urdf_path):
         rospy.loginfo(f"Waiting {compressed_urdf_path} from server")
         rospy.sleep(1.0)
-        gdown.cached_download(url=server_url,
-                              hash=f'md5:{urdf_hash}',
-                              path=compressed_urdf_path)
+        if StrictVersion(gdown_version) < StrictVersion("5.1.0"):
+            gdown.cached_download(url=server_url,
+                                  md5=f'{compressed_urdf_hash}',
+                                  path=compressed_urdf_path)
+        else:
+            gdown.cached_download(url=server_url,
+                                  hash=f'md5:{compressed_urdf_hash}',
+                                  path=compressed_urdf_path)
     gdown.extractall(osp.join(kxr_models_path, "models", "urdf", f"{urdf_hash}.tar.gz"))

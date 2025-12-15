4a4447ff74c4bcbb937a36fc8ec6ffc6e358d268
Iori Yanokura <ab.ioryz@gmail.com>
2025-12-15 23:11:09 +0900

    Fixed ipython (#217)

--- ros/kxr_controller/scripts/interface.py
+++ ros/kxr_controller/scripts/interface.py
@@ -6,7 +6,7 @@ import argparse
 import IPython
 from kxr_controller.check_ros_master import is_ros_master_local
 from kxr_controller.kxr_interface import KXRROSRobotInterface
-import numpy as np  # NOQA
+import numpy as np
 import rospy
 from skrobot.model import RobotModel
 
@@ -57,8 +57,16 @@ def main():
         else:
             raise NotImplementedError(f"Not supported viewer {args.viewer}")
 
-    # Drop into an IPython shell with both local & global vars
-    IPython.embed(global_ns=globals(), local_ns=locals())  # NOQA
+    # Drop into an IPython shell with variables accessible
+    user_ns = {
+        'robot_model': robot_model,
+        'ri': ri,
+        'np': np,
+        'rospy': rospy,
+    }
+    if args.viewer is not None:
+        user_ns['viewer'] = viewer
+    IPython.start_ipython(argv=[], user_ns=user_ns)
 
 
 if __name__ == "__main__":
